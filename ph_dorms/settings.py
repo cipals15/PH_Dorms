0e7848f840a67d4a69f6799852fb653fde879eba
cipals15 <cipals15@gmail.com>
2018-03-10 22:50:30 +0800

    Update settings.py
    removed Debug=True

--- ph_dorms/settings.py
+++ ph_dorms/settings.py
@@ -25,8 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
