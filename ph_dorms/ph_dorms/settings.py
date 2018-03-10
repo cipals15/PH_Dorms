9db51bca7f3a666771318df050e574a9afd6d883
cipals15 <cipals15@gmail.com>
2018-03-10 16:33:10 +0800

    Update settings.py
    replaced SECRET_KEY with config('SECRET_KEY')

--- ph_dorms/ph_dorms/settings.py
+++ ph_dorms/ph_dorms/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6uy5=$c2f^tw8oax%4tu_5!h6cyymlgxpu6yixbjkq$wvb8h7&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
