50d3c4157af9397a5f9b01474e4cf97fde36e164
cipals15 <cipals15@gmail.com>
2018-03-10 18:25:47 +0800

    Update settings.py
    add 'whitenoise.middleware.WhiteNoiseMiddleware' in Middleware

--- ph_dorms/ph_dorms/settings.py
+++ ph_dorms/ph_dorms/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	'social_django.middleware.SocialAuthExceptionMiddleware',
+	'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ph_dorms.urls'
