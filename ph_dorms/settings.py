480249c7745b6785cba11a242373f621bbe2d83a
cipals15 <cipals15@gmail.com>
2018-03-10 23:00:00 +0800

    Update settings.py
    add , to STATICFILES_DIRS

--- ph_dorms/settings.py
+++ ph_dorms/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-	os.path.join(PROJECT_ROOT,'static')
+	os.path.join(PROJECT_ROOT,'static'),
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'dorms/static/images/')
